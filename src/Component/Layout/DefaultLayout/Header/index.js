10d4c619063bdb6626d9230777d7ad061f5df367
Tuantai2000 <72971837+Tuantai2000@users.noreply.github.com>
2022-06-24 12:05:28 +0700

    complete header

--- src/Component/Layout/DefaultLayout/Header/index.js
+++ src/Component/Layout/DefaultLayout/Header/index.js
@@ -1,21 +1,30 @@
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-/* import image from "~/assets/images" */
+import images from "~/assets/images"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
-console.log(cx.wrapper);
 
 function Header() {
     return (
-        <header className={styles.wrapper}>
-            <div className={styles.topbar}>
-                <div>
-                    HEY THERE! ENJOY FREE SHIPPING ON ANY ORDER OF $150+ (OR FREE IN-STORE PICKUP)
-                </div>
+        <header className={cx('wrapper')}>
+            <div className={cx('topbar')}>
+                <div>HEY THERE! ENJOY FREE SHIPPING ON ANY ORDER OF $150+ (OR FREE IN-STORE PICKUP)</div>
             </div>
-            <div className={styles.inner}>
-                <div className={styles.mobileMenuButton}>
-                    <input className={styles.searchBar} placeholder='Search'></input>
+            <div className={cx('inner')}>
+                <div className={cx('mobile-menu-button')}>
+                    <div className={cx('search-bar')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        <input placeholder="Search"></input>
+                    </div>
+                </div>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Leif-shopify" style={{ height: '100%'}}/>
+                </div>
+                <div className={cx('shoping-cart')}>
+                    <a href='/description'>shopping cart</a>
+                    <FontAwesomeIcon icon={faBasketShopping} />
                 </div>
             </div>
         </header>
