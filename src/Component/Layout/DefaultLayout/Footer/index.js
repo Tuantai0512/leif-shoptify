85e3b4a1e24f5d156a78fc6539de0c947423f05f
Tuantai2000 <72971837+Tuantai2000@users.noreply.github.com>
2022-06-26 12:05:16 +0700

    Header end

--- src/Component/Layout/DefaultLayout/Footer/index.js
+++ src/Component/Layout/DefaultLayout/Footer/index.js
@@ -1,8 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './Footer.module.scss';
-import images from "~/assets/images"
-/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faBasketShopping } from '@fortawesome/free-solid-svg-icons'; */
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -70,15 +70,59 @@ function Footer() {
                             <img src={images.stamp} alt="leif_stamp" />
                         </div>
                         <div className={cx('info-section')}>
-                            <p><h4>Say hi:</h4></p>
+                            <p>
+                                <h4>Say hi:</h4>
+                            </p>
                             <p>Follow us @leifshop</p>
                             <p>hello@leifshop.com</p>
                         </div>
                     </div>
                 </div>
-                <div className={cx('newsletter-wraper')}></div>
+                <div className={cx('newsletter-wraper')}>
+                    <img src={images.stay} alt="stay-in-touch" style={{ width: '50%', marginLeft: '60px' }} />
+                    <div className={cx('footer-newsletter-from')}>
+                        <p className={cx('caption')}>
+                            Let's be friends — get 10% off your first order when you sign up for our emails.
+                        </p>
+                        <div className={cx('newsletter-from')}>
+                            <input className={cx('input-email')} placeholder={'Enter Email'} />
+                            <button className={cx('subscribe-button')}>subscribe</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={cx('footer-bottom')}>fottor bottom</div>
+            <div className={cx('footer-bottom')}>
+                <div className={cx('footer-section')}>
+                    <div className={cx('credit')}>
+                        <p>© 2022 LEIF, ALL RIGHTS RESERVED</p>
+                    </div>
+                </div>
+                <div className={cx('infos-wrapper')}>
+                    <p>
+                        <h2>
+                            Follow us
+                            <a href="https://www.facebook.com/">
+                                <FontAwesomeIcon icon={faFacebook} className={cx('mxh-icon')} />
+                            </a>
+                            <a href="https://www.instagram.com/">
+                                <FontAwesomeIcon icon={faInstagram} className={cx('mxh-icon')} />
+                            </a>
+                        </h2>
+                    </p>
+                </div>
+                <div className={cx('newsletter-wraper')}>
+                    <div className={cx('footer-newsletter-from')}>
+                        <div className={cx('info-section')}>
+                            <p>
+                                <a href="/">Privacy policy</a>
+                            </p>
+                            <p>
+                                <a href="/">Terms & condition</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,7 @@
 const images = {
     logo: require('~/assets/images/logo_leif.webp'),
-    stamp: require('~/assets/images/Leif_stamp.png')
+    stamp: require('~/assets/images/Leif_stamp.png'),
+    stay: require('~/assets/images/stay-in-touch.webp')
 };
 
 export default images;
